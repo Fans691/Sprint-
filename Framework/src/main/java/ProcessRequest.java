a89bab0e712d9d1f78a5081f324388a23d32a47c
Fans691 <fanilo.tsito@gmail.com>
2026-06-12 09:16:28 +0300

    sprint push

---
+++ Framework/src/main/java/ProcessRequest.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class ProcessRequest extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, IOException {
+            processRequest(req, res);
+        }
+
+        public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+            processRequest(req, res);
+        }
+
+        public void processRequest(HttpServletRequest req, HttpServletResponse res) 
+            throws ServletException, IOException {
+            String lien = req.getRequestURL().toString();
+            String[] parts = lien.split("/");
+            String lastPart = parts[parts.length - 1];
+            PrintWriter out = res.getWriter();
+
+            out.println("URL: " + lien);            
+        }
+    }
