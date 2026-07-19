ffffa8c9a437a2df3d457143dcfa3c8ff7f2908a
Fans691 <fanilo.tsito@gmail.com>
2026-07-19 19:53:28 +0300

    finalisation srint 5 bis

--- Framework/src/java/mg/itu/framework/util/MethodExecutor.java
+++ Framework/src/java/mg/itu/framework/util/MethodExecutor.java
@@ -3,7 +3,14 @@ package mg.itu.framework.util;
 public class MethodExecutor {
 
     public static Object execute(MethodClassMapping mapping) throws Exception {
-        Object instance = mapping.getClasse().getDeclaredConstructor().newInstance();
+        return execute(mapping, null);
+    }
+
+    public static Object execute(MethodClassMapping mapping, Object springContext) throws Exception {
+        Object instance = SpringContextManager.getBean(springContext, mapping.getClasse());
+        if (instance == null) {
+            instance = mapping.getClasse().getDeclaredConstructor().newInstance();
+        }
         mapping.getMethode().setAccessible(true);
         return mapping.getMethode().invoke(instance);
     }
